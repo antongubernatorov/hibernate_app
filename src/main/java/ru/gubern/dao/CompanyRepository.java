a0a57f59775493dd8dcc00a23463788616e6d84f
Anton <109098815+antongubernatorov@users.noreply.github.com>
2024-10-07 13:16:36 +0300

    refactor: добавлен комментарий

--- src/main/java/ru/gubern/dao/CompanyRepository.java
+++ src/main/java/ru/gubern/dao/CompanyRepository.java
@@ -7,5 +7,6 @@ import javax.persistence.EntityManager;
 public class CompanyRepository extends RepositoryBase<Long, Company> {
     public CompanyRepository(EntityManager entityManager) {
         super(Company.class, entityManager);
+        //todo
     }
 }
